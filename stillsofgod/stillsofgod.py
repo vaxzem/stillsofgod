08213795cb961a19871b4fbdcd1f61ae4bd8236d
Brimey <brimey@icloud.com>
2020-05-27 18:05:13 -0400

    Original file.

--- stillsofgod/stillsofgod.py
+++ stillsofgod/stillsofgod.py
@@ -12,5 +12,3 @@ ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-
-api.update_with_media('https://vgy.me/delete/5N3K20C2cnsC')
