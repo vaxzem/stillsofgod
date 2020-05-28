c96c8ffa8565c21f3a093cdc438e5473939c64cc
Brimey <brimey@icloud.com>
2020-05-28 09:04:27 -0400

    Added authentication into one function.

--- stillsofgod/stillsofgod.py
+++ stillsofgod/stillsofgod.py
@@ -4,15 +4,23 @@ import os
 import tweepy
 
 
-def get_env():
-    secrets = 'CONSUMER_KEY', 'CONSUMER_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET'
-    return {s: os.getenv(s, "Environment variable doesn't exist.") for s in secrets}
-
-
 def authenticate():
-    env_info = get_env()
-    auth = tweepy.OAuthHandler(env_info['CONSUMER_KEY'], env_info['CONSUMER_SECRET'])
-    auth.set_access_token(env_info['ACCESS_TOKEN'], env_info['ACCESS_TOKEN_SECRET'])
+    """Handles main authentication for Twitter account.
+
+    Returns:
+        API instance.
+    """
+    secrets = 'CONSUMER_KEY', 'CONSUMER_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET'
+
+    env_info = {s: os.getenv(s, "Environment variable doesn't exist.") for s in secrets}
+
+    auth = tweepy.OAuthHandler(
+        env_info['CONSUMER_KEY'], env_info['CONSUMER_SECRET']
+    )
+    auth.set_access_token(
+        env_info['ACCESS_TOKEN'], env_info['ACCESS_TOKEN_SECRET']
+    )
+
     return tweepy.API(auth)
 
 
